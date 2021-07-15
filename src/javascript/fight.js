4d71f9e9beca987435a299ab79261026c080b84e
Irina <irina.kbts@gmail.com>
2021-07-15 12:01:13 +0300

    Fighting is fixed

--- src/javascript/fight.js
+++ src/javascript/fight.js
@@ -1,14 +1,41 @@
 export function fight(firstFighter, secondFighter) {
     // return winner
-    return firstFighter;
+    while (true) {
+        if (firstFighter.health > 0) {
+            secondFighter.health = secondFighter.health - getDamage(firstFighter, secondFighter);
+        }
+        else {
+            return secondFighter;
+        }
+        if (secondFighter.health > 0) {
+            firstFighter.health = firstFighter.health - getDamage(secondFighter, firstFighter);
+        }
+        else {
+            return firstFighter;
+        }
+    }
 }
 export function getDamage(attacker, enemy) {
+    const damage = getHitPower(attacker) - getBlockPower(enemy);
+    if (damage < 0) {
+        return 0;
+    }
+    return damage;
     // damage = hit - block
     // return damage 
 }
+function getRandomIntInclusive(min, max) {
+    return Math.random() * (max - min + 1) + min;
+}
 export function getHitPower(fighter) {
+    const criticalHitChance = getRandomIntInclusive(1, 2);
+    const power = fighter.attack * criticalHitChance;
+    return power;
     // return hit power
 }
 export function getBlockPower(fighter) {
+    const dodgeChance = getRandomIntInclusive(1, 2);
+    const power = fighter.defense * dodgeChance;
+    return power;
     // return block power
 }
--- src/javascript/fight.ts
+++ src/javascript/fight.ts
@@ -1,20 +1,49 @@
-import { IFighter } from "./interfaces/IFighter";
 import { IFighterDetails } from "./interfaces/IFighterDetails";
 
-export function fight(firstFighter:IFighterDetails, secondFighter:IFighterDetails) {
+export function fight(firstFighter:IFighterDetails, secondFighter:IFighterDetails){
   // return winner
-  return firstFighter;
+  while(true){
+    if(firstFighter.health>0){
+      secondFighter.health = secondFighter.health - getDamage(firstFighter, secondFighter);
+    }
+    else{
+      return secondFighter;
+    }
+    if(secondFighter.health>0){
+      firstFighter.health = firstFighter.health - getDamage(secondFighter, firstFighter);
+    }
+    else{
+       return firstFighter;
+    }
+  }
+    
 }
 
-export function getDamage(attacker:IFighter, enemy:IFighter) {
+export function getDamage(attacker:IFighterDetails, enemy:IFighterDetails) {
+  const damage = getHitPower(attacker) - getBlockPower(enemy);
+  if(damage < 0)
+  {
+    return 0;
+  }
+  return damage;
   // damage = hit - block
   // return damage 
 }
 
-export function getHitPower(fighter:IFighter) {
+function getRandomIntInclusive(min:number, max:number) {
+  return Math.random() * (max - min + 1) + min;
+}
+
+export function getHitPower(fighter:IFighterDetails) {
+  const criticalHitChance = getRandomIntInclusive(1,2);
+  const power = fighter.attack * criticalHitChance;
+  return power;
   // return hit power
 }
 
-export function getBlockPower(fighter:IFighter) {
+export function getBlockPower(fighter:IFighterDetails) {
+  const dodgeChance = getRandomIntInclusive(1,2);
+  const power = fighter.defense * dodgeChance;
+  return power;
   // return block power
 }
